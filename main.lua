b1ea282c91ff7c1ffcd5bb11f76743c2f6c4e63a
Onyx <50289874+Tizitendo@users.noreply.github.com>
2024-10-10 15:31:09 +0200

    cleaned code and use initialize function now

--- main.lua
+++ main.lua
@@ -18,29 +18,31 @@ StarCount = 0
 BufferedUtil = 0
 player = nil
 
-function setup_arti()
-    player = Player.get_client()
-    local artiC2 = Skill.find("ror-artiC2")
-    local artiV2 = Skill.find("ror-artiX2")
-    local artiV2Boosted = Skill.find("ror-artiV2Boosted")
-    artiC2.cooldown = 480.0
-    artiV2.cooldown = 300.0
-    artiV2Boosted.cooldown = 300.0
+__initialize = function()
+    Artificer = Survivor.find("ror-arti")
 
-    local speed_multi = 2.0
-    gm.sprite_set_speed(gm.constants.sArtiShoot1_1A, speed_multi, 1)
-    gm.sprite_set_speed(gm.constants.sArtiShoot1_2A, speed_multi, 1)
-    gm.sprite_set_speed(gm.constants.sArtiShoot1_1B, speed_multi, 1)
-    gm.sprite_set_speed(gm.constants.sArtiShoot1_2B, speed_multi, 1)
-end
-
-gm.pre_script_hook(gm.constants.callback_execute, function(self, other, result, args)
-    if args[1].value == 26 and self.class == 13.0 then -- onPlayerStep
+    Artificer:onInit(function(self)
+        player = Player.get_client()
+        local artiC2 = Skill.find("ror-artiC2")
+        local artiV2 = Skill.find("ror-artiX2")
+        local artiV2Boosted = Skill.find("ror-artiV2Boosted")
+        artiC2.cooldown = 480.0
+        artiV2.cooldown = 300.0
+        artiV2Boosted.cooldown = 300.0
+    
+        local speed_multi = 2.0
+        gm.sprite_set_speed(gm.constants.sArtiShoot1_1A, speed_multi, 1)
+        gm.sprite_set_speed(gm.constants.sArtiShoot1_2A, speed_multi, 1)
+        gm.sprite_set_speed(gm.constants.sArtiShoot1_1B, speed_multi, 1)
+        gm.sprite_set_speed(gm.constants.sArtiShoot1_2B, speed_multi, 1)
+    end)
+    
+    Artificer:onStep(function(self)
         -- Hover
         if self.moveUpHold == 1.0 and self.pVspeed > 0.0 then
             self.pVspeed = self.pVspeed * 0.9
         end
-
+    
         -- hold special to slow down + artistar control
         for i = 0, 4 do
             if artistar[i] ~= nil then
@@ -52,35 +54,29 @@ gm.pre_script_hook(gm.constants.callback_execute, function(self, other, result, 
                 end
             end
         end
-
+    
         -- increase surge distance
         if BufferedUtil > 0 and player:get_skill(2).identifier == "artiC2" then
             BufferedUtil = 0
             self.pHspeed = self.pHspeed * 1.5
             self.pVspeed = self.pVspeed * 1.2
         end
-    end
+    end)
 
-    -- Skill setup
-    if args[1].value == 25 then -- onPlayerInit
-        setup_arti()
-    end
-end)
-
--- check if util is used
-gm.post_script_hook(gm.constants._skill_system_update_skill_used, function(self, other, result, args)
-    if self.class == 13.0 and self.c_skill == true then
-        BufferedUtil = 1
-    end
-end)
-
--- arti star better control
-gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, result, args)
-    if result.value.object_index == gm.constants.oEfArtiStar then
-        artistar[StarCount] = result.value
-        StarCount = StarCount + 1
-        if StarCount > 3 then
-            StarCount = 0
+    gm.post_script_hook(gm.constants._skill_system_update_skill_used, function(self, other, result, args)
+        if self.class == 13.0 and self.c_skill == true then
+            BufferedUtil = 1
         end
-    end
-end)
+    end)
+    
+    -- arti star better control
+    gm.post_script_hook(gm.constants.instance_create_depth, function(self, other, result, args)
+        if result.value.object_index == gm.constants.oEfArtiStar then
+            artistar[StarCount] = result.value
+            StarCount = StarCount + 1
+            if StarCount > 3 then
+                StarCount = 0
+            end
+        end
+    end)
+end
